f8611b82874ed179cf5efb3e657e37d5e4e7b05b
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2024-05-21 13:57:33 +0000

    update

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -8,7 +8,7 @@ export class MainMenu extends Scene
 
     constructor ()
     {
-        super('Hello, World!');
+        super('MainMenu');
     }
 
     create ()
@@ -17,7 +17,7 @@ export class MainMenu extends Scene
 
         this.logo = this.add.image(512, 300, 'logo');
 
-        this.title = this.add.text(512, 460, 'Hello, World!', {
+        this.title = this.add.text(512, 460, 'Main Menu', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
