1c406d39e39ca3672ec25ac2e766afa5dd8e9e0d
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2024-06-06 13:57:28 +0000

    update

---
+++ src/game.ts
@@ -0,0 +1,29 @@
+import "phaser";
+import { GameScene } from "./scenes/game-scene";
+
+const config: GameConfig = {
+    width: 390,
+    height: 600,
+    parent: "game",
+    scene: [GameScene],
+    input: {
+      keyboard: true
+    },
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 300 }
+      }
+    },
+    render: { pixelArt: true }
+  };
+  
+  export class Game extends Phaser.Game {
+    constructor(config: GameConfig) {
+      super(config);
+    }
+  }
+  
+  window.addEventListener("load", () => {
+    let game = new Game(config);
+  });
